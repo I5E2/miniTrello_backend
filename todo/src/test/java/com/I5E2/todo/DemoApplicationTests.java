daa2ae4d5194065a0d3944721b9d0ea6fec282a9
pro_k9460(강현철) <pro_k9460@naver.com>
2022-09-24 09:36:40 +0900

    프로젝트 최초 생성

---
+++ todo/src/test/java/com/I5E2/todo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I5E2.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
