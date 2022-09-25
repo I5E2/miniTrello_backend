69d3f0ac874f79e609745519191cfc788693c163
BuildTools <unconfigured@null.spigotmc.org>
2022-09-25 17:15:39 +0900

    시큐리티 로그인 창 무시

--- todo/src/main/java/com/I5E2/todo/DemoApplication.java
+++ todo/src/main/java/com/I5E2/todo/DemoApplication.java
@@ -2,8 +2,9 @@ package com.I5E2.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
